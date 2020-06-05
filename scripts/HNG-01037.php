601757a576645d8477dfe4a265759772b8ad3521
Basheeroh M. Akinpelu <52384451+basheeroh@users.noreply.github.com>
2020-06-05 22:12:10 +0100

    update HNG-01037

--- scripts/HNG-01037.php
+++ scripts/HNG-01037.php
@@ -1,7 +1,8 @@
   
-<?php
+<<?php
     $fullname = "Akinpelu Basheeroh";
     $hngid = "HNG-01037";
     $language = "PHP";
-        echo 'Hello World, this is '.$fullname.' with HNGi7 ID '.$hngid.' using '.$language.' for stage 2 task.'; 
+    $email = "basheeroh22gmail.com";
+        echo 'Hello World, this is '.$fullname.' with HNGi7 ID '.$hngid.' using '.$language.' for stage 2 task. '.$email.''; 
         ?>
